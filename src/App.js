18e5bde4941e233d43acebd744544f310578f74e
Olorunsogo Banwo <sogobanwo@gmail.com>
2023-12-16 17:45:09 +0100

    Feat: Added all neccessary features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Quiz from "./Pages/Quiz";
 
 function App() {
   return (
+    // <HashRouter>
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Quiz />} />
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default App
